6d4d0ef6c64a90352bebc3fa2939cf99d12ed712
Kim Sehyun <kkshyun56@gmail.com>
2024-05-21 11:49:26 +0900

    test-dbUserNameIsSa()

--- src/test/java/com/example/devops/DevopsApplicationTests.java
+++ src/test/java/com/example/devops/DevopsApplicationTests.java
@@ -1,13 +1,21 @@
 package com.example.devops;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class DevopsApplicationTests {
-
+	@Value("${spring.datasource.username}")
+	String username;
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void dbUserNameIsSa(){
+		Assertions.assertEquals("sa",username);
 	}
 
 }
